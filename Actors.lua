c2a1f2613db49f671d85783d8fbd02eb943f97fd
Mateusz Stanisław Kapłon <mateusz.kaplon@gmail.com>
2013-09-03 21:11:47 +0200

    Patch on collision detection.
    It works better than ever now.

--- Actors.lua
+++ Actors.lua
@@ -35,9 +35,8 @@ local Collidable = class {
 	-- dr - moving offset
 	-- collision_fn - boolean function checking if there's a collision going
 	-- object_size - object's size in given dimention
-	-- tile_size - tile's size in given dimention
 	-- RETURN VALUE: <bool indicating if there is a collision to resolve>, <resolved position>
-	resolve_collision = function (self, pos, dr, object_size, tile_size, collision_fn)
+	resolve_collision = function (self, pos, dr, object_size, collision_fn)
 		local function clamp(val)
 			if math.abs(val) > 1 then
 				return math.abs(val) / val
@@ -46,9 +45,13 @@ local Collidable = class {
 			end
 		end
 
+		-- Maximum distance object can go in one iteration, in pixels
+		local step = 40
+
 		local t = 1
 		local s = object_size / 2
 		local round = math.floor
+		dr = dr / step
 		if dr < 0 then
 			dr = math.abs(dr)
 			t = -t
@@ -56,7 +59,7 @@ local Collidable = class {
 			round = math.ceil
 		end
 		while dr ~= 0 do
-			local newpos = pos + clamp(dr) * t * tile_size
+			local newpos = pos + clamp(dr) * t * step
 			if collision_fn(newpos + s) then
 				local delta = round(newpos - pos)
 				while collision_fn(pos + delta + s) do
@@ -69,34 +72,35 @@ local Collidable = class {
 				if dr < 0 then dr = 0 end
 			end
 		end
-		return false, round(pos)
+		return false, pos
 	end;
 
 	onUpdate = function (self, dt)
 		local map = self.map
 		local x, y = self.x, self.y
-		local w, h = self.width / 2, self.height / 2
+		local w, h = self.width, self.height
 
 		self.map:setCurrentCollidable(self)
 
 		-- Gravity first of all.
-		if not map:hitTest(x - w + 1, y + h) and not map:hitTest(x + w - 1, y + h) then
+		if not map:hitTest(x - w / 2 + 1, y + h / 2) and not map:hitTest(x + w / 2 - 1, y + h / 2) then
 			self.v = self.v + Config.Gravity * dt
 			self.falling = true
 		end
 
-		-- I hate this part, but it has to be done.
-		local x_collision, x = self:resolve_collision(
-			x, self.v.x * dt / map.tileWidth,
-			self.width, map.tileWidth,
-			function (pos) return map:hitTest(pos, y) end)
+		local hitTestX = function (x)
+			return map:hitTest(x, y - h / 2 + 1) or map:hitTest(x, y + h / 2 - 1)
+		end
+		local hitTestY = function (y)
+			return map:hitTest(x - w / 2 + 1, y) or map:hitTest(x + w / 2 - 1, y)
+		end
 
-		local y_collision, y = self:resolve_collision(
-			y, self.v.y * dt / map.tileHeight,
-			self.height, map.tileHeight,
-			function (pos) return map:hitTest(x, pos) end)
+		local x_collision, x = self:resolve_collision(x, self.v.x * dt, w, hitTestX)
+		local y_collision, y = self:resolve_collision(y, self.v.y * dt, h, hitTestY)
 
-		if x_collision == true then self.v.x = 0 end
+		if x_collision == true then
+			self.v.x = 0
+		end
 		if y_collision == true then
 			self.v.y = 0
 			self.falling = false
